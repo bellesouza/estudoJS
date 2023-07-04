8bbd27743ee75dee4aab3ad95cb1f9a21042a93a
bellesouza <127145848+bellesouza@users.noreply.github.com>
2023-07-04 09:55:31 -0300

    Add files via upload

--- 7-Condicionais.js
+++ 7-Condicionais.js
@@ -1,29 +1,61 @@
 console.log("trabalhando com condicionais");
 const listaDeDestinos = new Array(
-    
-'Salvador',
-'São Paulo', 
-'Rio de Janeiro'
+
+    'Salvador',
+    'São Paulo',
+    'Rio de Janeiro'
 
 )
-const idadeComprador = 21;
-console.log ("destinos possiveis");
+const idadeComprador = 17;
+const estaacompanhada = true;
 
-if ( idadeComprador >= 18){
-    console.log ("comprador maior de idade");
-    listaDeDestinos.splice(1,1);
+console.log("destinos possiveis");
+console.log(listaDeDestinos);
+
+// if ( idadeComprador >= 18){
+//     console.log ("comprador maior de idade");
+//     listaDeDestinos.splice(1,1); //removendo item
+
+// }
+// else{
+//     //a pessoa é maior de idade
+//     if(estaacompanhada){
+//         console.log ("comprador esta acompanhado");
+//     listaDeDestinos.splice(1,1);
+
+
+//     }else{
+//         console.log("nao é maior de idade e nao posso vender");
+// }
+
+//     }
+
+if (                                      //arrumar erro
+    idadeComprador >= 18
+    estaacompanhada == true
+    ) {
+    console.log("comprador maior de idade");
+    listaDeDestinos.splice(2, 1); //removendo item
 
 }
-else{
+else {
+    //a pessoa é maior de idade
+    else if () {
+        console.log("comprador esta acompanhado");
+        listaDeDestinos.splice(2, 1);
 
-console.log("nao é maior de idade e nao posso vender");
+
+    } else {
+        console.log("nao é maior de idade e nao posso vender");
+    }
+
 }
 
 
 console.log(listaDeDestinos);
 
-console.log( idadeComprador >18);
-console.log(idadeComprador < 18);
-console.log( idadeComprador <=18);
-console.log( idadeComprador >=18);
-console.log( idadeComprador ==18);
+// console.log( idadeComprador >18);
+// console.log(idadeComprador < 18);
+// console.log( idadeComprador <=18);
+// console.log( idadeComprador >=18);
+// console.log( idadeComprador ==18);
